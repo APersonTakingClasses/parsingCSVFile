2f46b637801991ac33bb3d685c753d7282ed6474
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2022-01-26 12:28:48 -0500

    Created git repo, forgot to make repo earlier.
    - Downloaded excel file csv
    - Trying to figure out how to add dependency correctly

---
+++ src/main/java/parseCSV.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class parseCSV {
+    public static void main(String[] args){
+        //greeting message
+        System.out.println("\n---Parse CSV File Code---\n");
+        // make parser
+        CSVParser parser = CSVFormat.EXCEL.parse(reader);
+        // grab file to parse
+        // do parsing
+
+        // display parsed info
+
+        // parsed info calculations
+    }
+
+}
