0c1ec20089d75722fb18996234c8aa50f084df75
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2022-01-26 12:47:37 -0500

    - Fixed dependancy thing - writing parser, for specific file path only - trying to format

--- src/main/java/parseCSV.java
+++ src/main/java/parseCSV.java
@@ -1,12 +1,27 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class parseCSV {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         //greeting message
         System.out.println("\n---Parse CSV File Code---\n");
+
         // make parser
-        CSVParser parser = CSVFormat.EXCEL.parse(reader);
-        // grab file to parse
+        File data = new File("C:\\Users\\firew\\IdeaProjects\\parsingCSVFile\\real-estate-data.csv");
+        CSVParser parser = CSVParser.parse(data, StandardCharsets.US_ASCII, CSVFormat.EXCEL);
+
+        CSVFormat fmt = CSVFormat.EXCEL.withDelimiter(',');
+        /**street,city,zip,state,beds,
+         * baths,sq__ft,type,sale_date,
+         * price,latitude,longitude**/
+        CSVFormat.EXCEL.withHeader("street", "city", "zip", "state", "beds",
+                "baths", "sq__ft", "type", "sale_date",
+                "price", "latitude", "longitude");
         // do parsing
 
         // display parsed info
