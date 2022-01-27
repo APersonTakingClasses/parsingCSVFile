288a24cfb7bd3e4672576c9681b0a89935fd87ee
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2022-01-26 21:56:35 -0500

    IT WORKS XD
    I just needed to ask a human being who understood the library fml
    
    uses clunky for loop to summate and compare for min/max
    looks ugly as hell

--- src/main/java/parseCSV.java
+++ src/main/java/parseCSV.java
@@ -16,37 +16,40 @@ public class parseCSV {
         File data = new File("C:\\Users\\firew\\IdeaProjects\\parsingCSVFile\\real-estate-data.csv");
         File excelFile = new File("C:\\Users\\firew\\IdeaProjects\\parsingCSVFile\\workbook.xlsx");
 
-        CSVParser parser = CSVParser.parse(data, StandardCharsets.US_ASCII, CSVFormat.EXCEL);
-
+        CSVParser parser = CSVParser.parse(data, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
+        //CSVParser parser = CSVParser.parse(data, CSVFormat.DEFAULT);
         // do parsing
-        CSVFormat.EXCEL.withDelimiter(',').withHeader("street", "city", "zip", "state", "beds", "baths", "sq__ft", "type", "sale_date", "price", "latitude", "longitude");
+        //CSVFormat.EXCEL.withDelimiter(',').withHeader("street", "city", "zip", "state", "beds", "baths", "sq__ft", "type", "sale_date", "price", "latitude", "longitude");
         /**street,city,zip,state,beds,
          * baths,sq__ft,type,sale_date,
          * price,latitude,longitude**/
-        List<String> headers;
-        List<String> prices = List.of(new String[]{""});
-        List<String> sqft;
-
         List<CSVRecord> allInfo = parser.getRecords();
 
+        List<String> headers;
+        Double prices = 0.0;
+        Double maxP = 0.0;
+        Double minP = Double.valueOf(allInfo.get(1).get(9));
+        Double sqft = 0.0;
+
         headers = allInfo.get(0).toList();
-        //prices = allInfo.get().stream().toList();
-        int i = 1;
-        String temp = "";
-        List<String> record = allInfo.get(0).toList();
-        while(i<986){
-            record = allInfo.get(i).toList();
-            temp = record.get(10).substring(1,2);
-            prices.add(temp.toString());
-            i++;
+        //prices = Double.valueOf(allInfo.get(1).get(9));
+
+        for(int i = 1; i<986; i++){
+            prices += Double.valueOf(allInfo.get(i).get(9));
+            sqft += Double.valueOf(allInfo.get(i).get(6));
+            if(minP > Double.valueOf(allInfo.get(i).get(9))) minP = Double.valueOf(allInfo.get(i).get(9));
+            if(maxP < Double.valueOf(allInfo.get(i).get(9))) maxP = Double.valueOf(allInfo.get(i).get(9));
         }
+        prices /= 986;
+        sqft /= 986;
+
         //prices = Collections.singletonList(allInfo.get(Integer.parseInt("price")).toString());
 
         System.out.println("Headers: "+headers);
         System.out.println("Avg Prices: "+prices);
-        System.out.println("Avg Sq__Ft: ");
-        System.out.println("Min Price: ");
-        System.out.println("Max Prices: ");
+        System.out.println("Avg Sq__Ft: "+sqft);
+        System.out.println("Min Price: "+minP);
+        System.out.println("Max Price: "+maxP);
     }
 
 }
